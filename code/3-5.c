6cbb4a71e0c1b479a6af5d3e331992a37b42c7c1
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-03-20 15:00:01 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char c, int a,...){
+    int sum = 0;
+    va_list ap;
+
+    va_start(ap, a);
+
+    for(int i = 0; i<a; i++){
+        if(c == 'S'){
+            char* val = va_arg(ap, char*);
+            printf("%s",val);
+            printf(" ");
+        }else if(c == 'C'){
+            char val = va_arg(ap, int);
+            printf("%c",val);
+        }else if(c == 'D'){
+            int val = va_arg(ap, int);
+            sum += val;
+        }
+        
+    }
+    if(c == 'D')
+            printf("%d",sum);
+
+    va_end(ap);
+    printf("\n");
+}
+
+int main(){
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
